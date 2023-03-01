31aea4dd72db5a7fea3fbc19edc5f3932bc30f88
pustot <maki.nwd@gmail.com>
2023-03-01 22:01:26 +0300

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+       <h1>Weather App</h1>
+       <h3>Under construction</h3>
+    </div>
+  );
+}
+
+export default App;
